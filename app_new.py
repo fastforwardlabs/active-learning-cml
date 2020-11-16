614f7c458c30e2e2346ac2ecbb2d733182befd60
Nisha Muktewar <nisha@cloudera.com>
2020-11-16 22:33:59 +0000

    learning dash

--- app_new.py
+++ app_new.py
@@ -7,15 +7,20 @@ from demo import create_layout, demo_callbacks
 # for the Local version, import local_layout and local_callbacks
 # from local import local_layout, local_callbacks
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+'''
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
-
-server = app.server
+'''
+#server = app.server
 app.layout = create_layout(app)
 demo_callbacks(app)
 
 # Running server
 if __name__ == '__main__':
     # strategy = EntropySampling(X, Y, X_TE, Y_TE, X_NOLB, net, handler, args)
-    app.run_server(port=os.getenv("CDSW_APP_PORT"), debug=True)
+    
+    app.run_server(port=os.getenv("CDSW_APP_PORT"))
